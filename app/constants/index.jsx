438beeaecd99b4d523f17924234a7e69a46cc908
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-05-14 10:34:53 +0200

    Create Info Barrell
    Created a barrelfile to contain all my good to have info

---
+++ app/constants/index.jsx
@@ -0,0 +1,35 @@
+export const personalInfo = {
+	firstname: "Rasmus",
+	lastname: "Bremholm",
+	subheading: "Frontend Developer",
+	email: "rasmus.brem@gmail.com",
+	tel: "0762693031",
+};
+
+export const skills = [
+	{
+		id: 1,
+		name: "Webdevelopment",
+		img: "null",
+	},
+	{
+		id: 2,
+		name: "NextJS 14",
+		img: "null",
+	},
+	{
+		id: 3,
+		name: "React",
+		img: "null",
+	},
+	{
+		id: 4,
+		name: "ThreeJS",
+		img: "null",
+	},
+	{
+		id: 5,
+		name: "Supabase",
+		img: "null",
+	},
+];
